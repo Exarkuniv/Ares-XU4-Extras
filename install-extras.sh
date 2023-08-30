41f2f707ef230a1ceb09d44a5024db8b8f023b50
Exarkuniv <48326368+Exarkuniv@users.noreply.github.com>
2023-08-30 09:38:19 -0500

    Update install-extras.sh

--- install-extras.sh
+++ install-extras.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-RPS_HOME='/home/pi/RetroPie-Setup'
+RPS_HOME='/home/aresuser/ARES-Setup'
 if [ ! -z "$1" ];then
     RPS_HOME="$1"
 fi
 if [ ! -d "$RPS_HOME" ];then
-    echo "RetroPie-Setup directory $RPS_HOME doesn't exist. Please input the location of RetroPie-Setup."
-    echo "EX: ./install-extras.sh /home/pi/RetroPie-Setup"
+    echo "RetroPie-Setup directory $RPS_HOME doesn't exist. Please input the location of Ares."
+    echo "EX: ./install-extras.sh /home/aresuser/ARES-Setup"
     exit
 fi
 
